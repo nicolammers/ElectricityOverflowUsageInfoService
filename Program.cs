5c66f722803abe4ee2d72ed2ad83642d3c5ed941
Nico Lammers <nico.lammers@amcon.de>
2022-09-20 15:03:57 +0200

    Init Project

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ElectricityOverflowUsageInfoService
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
