90846baa3e527c14d70b60f4d2d362b88522af9f
Nico Lammers <nico.lammers@amcon.de>
2022-09-21 10:55:05 +0200

    -

--- ProgramStart.cs
+++ ProgramStart.cs
@@ -1,13 +1,46 @@
 ﻿using ElectricityOverflowUsageInfoService.SmardApi.DTO;
+using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace ElectricityOverflowUsageInfoService {
     internal class ProgramStart {
         static async Task Main(string[] args) {
-           Indices indices = await SmardApi.SmardApiReader.GetIndicesAsync();
+            Indices indices = await (new SmardApi.SmardApiReader()).GetIndicesAsync();
+            indices.Timestamps.Reverse();
 
-           await SmardApi.SmardApiReader.GetTimeSeriesAsync(indices.Timestamps.First());
+            //foreach (double mainTs in indices.Timestamps) {
+                Console.WriteLine("Indices: " + UnixTimeStampToDateTime(indices.Timestamps.First()).ToString());
+
+                TimeSeries timeSeries = await (new SmardApi.SmardApiReader()).GetTimeSeriesAsync(indices.Timestamps.First());
+
+                if(timeSeries != null && timeSeries.Series.Any()) {
+                    foreach (List<double?> element in timeSeries.Series) {
+                        Console.WriteLine("                  " + UnixTimeStampToDateTime((double) element[0]));
+                        Console.WriteLine("                  " + element[1].ToString());
+
+                        Console.WriteLine("");
+                        Console.WriteLine("");
+                    }
+                } else if (timeSeries != null && !timeSeries.Series.Any()) {
+                    Console.WriteLine("No Subdata but Series found");
+                } else {
+                    Console.WriteLine("No Series found for Timestamp");
+                }
+
+            //    Console.WriteLine("");
+            //    Console.WriteLine("");
+            //    Console.WriteLine("");
+            //    Console.WriteLine("");
+            //    Console.WriteLine("");
+            //}
+        }
+
+        public static DateTime UnixTimeStampToDateTime(double unixTimeStamp) {
+            System.DateTime dtDateTime = new DateTime(1970, 1, 1, 0, 0, 0, 0, System.DateTimeKind.Utc);
+            dtDateTime = dtDateTime.AddMilliseconds(unixTimeStamp).ToLocalTime();
+            return dtDateTime;
         }
     }
 }
--- SmardApi/SmardApiReader.cs
+++ SmardApi/SmardApiReader.cs
@@ -7,14 +7,14 @@ using System.Threading.Tasks;
 namespace ElectricityOverflowUsageInfoService.SmardApi {
     public class SmardApiReader {
 
-        public static string APIGATEWAY = "https://www.smard.de/app/chart_data";
+        private readonly string APIGATEWAY= "https://www.smard.de/app/chart_data";
 
-        public async static Task<Indices> GetIndicesAsync() {
+        public async Task<Indices> GetIndicesAsync() {
             try {
                 using (HttpClient client = new HttpClient()) {
                     //ToDO: Timeout of CLient
 
-                    HttpResponseMessage response = await client.GetAsync(new Uri(APIGATEWAY + "/410/DE/index_hour.json"));
+                    HttpResponseMessage response = await client.GetAsync(new Uri(APIGATEWAY + "/1225/DE/index_hour.json"));
                     string contentString = await response.Content.ReadAsStringAsync();
                     Indices indices = JsonConvert.DeserializeObject<Indices>(contentString);
                     return indices;
@@ -25,14 +25,11 @@ namespace ElectricityOverflowUsageInfoService.SmardApi {
             }
         }
 
-        public async static Task<TimeSeries> GetTimeSeriesAsync(double timestamp) {
+        public async Task<TimeSeries> GetTimeSeriesAsync(double timestamp) {
             try {
                 using (HttpClient client = new HttpClient()) {
-                    HttpResponseMessage response = await client.GetAsync(new Uri(APIGATEWAY + "/410/DE/410_DE_hour_" + timestamp.ToString() + ".json"));
+                    HttpResponseMessage response = await client.GetAsync(new Uri(APIGATEWAY + "/1225/DE/1225_DE_quarterhour_" + timestamp.ToString() + ".json"));
                     string contentString = await response.Content.ReadAsStringAsync();
-
-                    Console.WriteLine(contentString);
-
                     TimeSeries timeSeries = JsonConvert.DeserializeObject<TimeSeries>(contentString);
                     return timeSeries;
                 }
