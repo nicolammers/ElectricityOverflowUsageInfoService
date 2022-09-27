b86a3e269b388e20462fbe7d8b206ba0c79e226f
Nico Lammers <nico.lammers@amcon.de>
2022-09-27 10:02:32 +0200

    CodeSmell

--- ElectricityOverflowUsageApi/Program.cs
+++ ElectricityOverflowUsageApi/Program.cs
@@ -13,10 +13,11 @@ namespace ElectricityOverflowUsageApi {
             CreateHostBuilder(args).Build().Run();
         }
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder => {
-                    webBuilder.UseStartup<Startup>();
-                });
+        public static IHostBuilder CreateHostBuilder(string[] args) {
+            return Host.CreateDefaultBuilder(args)
+                       .ConfigureWebHostDefaults(webBuilder => {
+                           webBuilder.UseStartup<Startup>();
+                       });
+        }
     }
 }
